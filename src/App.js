7a21d6c5fa6f4c50c14972eb4e2b30c89deb9fd5
wirsodev <wilson.wmarques@gmail.com>
2021-02-18 18:50:39 +0000

    start build login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Login from './components/Login'
-
+import env from "react-dotenv"
+import { useState } from 'react'
 
 const App = () =>{
-  return(
-    <Login />
-  )
-}
+  
+  const [code, setCode] = useState('')
+  const [isValid, setIsValid] = useState(false)
+  const [logerror, setLogError] = useState(false)
 
+  function checkCode(e){
+    e.preventDefault()
+    
+    if(code == env.CODE){
+      setIsValid(true)
+    }else{
+      setLogError(true)
+      setCode('')
+    }
+  }
+  if(!isValid){
+
+    return(
+      <div class="app">
+        <Login 
+          onSubmit={checkCode}
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+          errorMessage={ logerror ? 'NOP' : ''}
+        /> 
+      </div>
+     )
+  }else{
+    return(
+      <h1>You are in!</h1>
+    )
+  }
+}
 
 export default App
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,20 @@
 import '../styles/login.css'
-import React, { useState } from "react";
+import { useState } from 'react'
 
-const Login = () => {
+const Login = ( props ) => {
 
-    const [value, setValue] = useState('')
-
-
-    function checkCode(){
-
-        if(value == 1234){
-            alert('IN')
-        }else{
-            alert('nop')
-        }
-    }
-    
     return(
         <div className="login">
-            <form onSubmit={checkCode}>
+            <form onSubmit={props.onSubmit}>
                 <h1>Enter code:</h1>
                 <input 
-                type="text" 
-                value={value} 
+                type="password" 
+                value={props.value} 
                 placeholder='your code here'
-                onChange={(e) => setValue(e.target.value)}/>
+                onChange={props.onChange}
+                />
                 <button>submit</button>
+                <p>{props.errorMessage}</p>
             </form>
         </div>
     )
